7d3757c808a3fa65f892427c0d882290c84715b5
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-20 19:51:00 +0400

    Create Homework15.3.cpp

---
+++ Homework15.3/Homework15.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void sorting( vector <int> array) {
+    int temp;
+    for (int i = 0; i < array.size(); ++i) {
+        for (int j = 0; j < array.size() - (i + 1); ++j) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    cout << array[4] << endl;
+}
+
+int main(){
+    int number;
+    vector <int> array;
+    do {
+        cout << "Enter a number: ";
+        cin >> number;
+        if (number == -1) sorting(array);
+        else array.push_back(number);    
+    } while (number != -2); 
+}
