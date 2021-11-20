ebcda772d20e37715f42ef226f3f30a66b622cfc
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-20 18:56:17 +0400

    Create Homework 15.1.cpp

---
+++ Homework 15.1/Homework 15.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int mass[10] = {-2,1,-3,4,-1,2,1,-5,4,-3};
+    int maxInd = 9;
+    int minInd = 0;
+    int max = 0;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = i + 1; j < 9; ++j) {
+            int temp = 0;
+            for (int k = i; k <= j; ++k) {
+                temp += mass[k];
+            }
+            if (temp > max) {
+                max = temp;
+                maxInd = j;
+                minInd = i;
+            }
+        }
+    }
+    std::cout << "Max =" << max << ", i =" << minInd << ", j =" << maxInd;
+}
