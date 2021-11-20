392a49a6d49396fc4910cf2a117511e8353c7243
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-20 20:50:26 +0400

    Create Homework15.4.cpp

---
+++ Homework15.4/Homework15.4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int array[6] = { -100,-50,-5,1,10,15 };
+    int temp;
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 6 - (i + 1); ++j) {
+            if (abs(array[j]) > abs(array[j + 1])) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 6; ++i) {
+        std::cout << array[i] << ", ";
+    }
+}
