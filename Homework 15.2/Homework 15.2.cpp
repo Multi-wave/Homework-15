f35eca00a35a216c2a2ecbfe9c280ec7a1ef0f98
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-11-20 19:05:17 +0400

    Create Homework 15.2.cpp

---
+++ Homework 15.2/Homework 15.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    int mass[4] = {2,7,11,15};
+    int result = 9;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = i + 1; j < 4; ++j) {
+            if ((mass[i] + mass[j]) == result) std::cout << "Number1 = " << mass[i] << ", number2 = " << mass[j];
+        }
+    }
+}
